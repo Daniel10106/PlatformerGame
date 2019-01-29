86630898c5ded8df835f40fda462dfcef83d275b
Daniel10106 <dani.10103m@gmail.com>
2019-01-29 12:00:49 +0200

    Upload

---
+++ SimplePhysics/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace SimplePhysics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picBall = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBall
+            // 
+            this.picBall.BackColor = System.Drawing.Color.Aqua;
+            this.picBall.Location = new System.Drawing.Point(62, 182);
+            this.picBall.Name = "picBall";
+            this.picBall.Size = new System.Drawing.Size(30, 30);
+            this.picBall.TabIndex = 0;
+            this.picBall.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 15;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.picBall);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBall;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ SimplePhysics/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimplePhysics
+{
+    public partial class Form1 : Form
+    {
+        public double vSpeed;
+        public double hSpeed;
+        public bool movingRight;
+        public bool jumping;
+        public bool grounded;
+
+        public Form1()
+        {
+            vSpeed = 0;
+            hSpeed = 0;
+            movingRight = true;
+            InitializeComponent();
+        }
+
+        private void ballMove()
+        {
+            //fix the jump up part
+            picBall.Top += (int)vSpeed;
+            picBall.Left += (int)hSpeed;
+
+            if (jumping)
+            {
+                vSpeed += 0.2;
+            }
+            if (picBall.Bottom > 300) { grounded = true; }
+            if (picBall.Right >= this.ClientSize.Width) { hSpeed = -hSpeed; movingRight = !movingRight; }
+            if (picBall.Left < 0) { hSpeed = -hSpeed; }
+            if (grounded) { vSpeed = 0; }
+            if (vSpeed < 0 && vSpeed > -0.4) { vSpeed = 0.4; }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ballMove();
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                hSpeed = -2;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                hSpeed = 2;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                grounded = false;
+                jumping = true;
+                vSpeed = 4;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                hSpeed = 0;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                hSpeed = 0;
+            }
+        }
+    }
+}
